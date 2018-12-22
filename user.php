095c8edbe886704ffc91e3c00e2f07efa5be90d6
haichaulightning <45975564+haichaulightning@users.noreply.github.com>
2018-12-22 21:28:04 +0700

    Add files via upload

---
+++ user.php
@@ -0,0 +1,18 @@
+<?php
+echo $red."[!] Chú Ý !!!".$t;
+echo $kuning."[-] Sử Dụng Tool Bot Veeu Có Thể Bị Không Rút Tiền Về Được".$t;
+echo $kuning."[-] Tôi Không Chịu Trách Nhiệm Về Việc Sử Dụng Tool Bot Veeu".$t.$ijo.$t;
+$ads= $putih."[÷]    official channel    [÷]".$t.$ijo."[-] Make Money Online 4.0 ".$putih."[-]";
+$msgtoto = "[!] Subscribe Youtube Make Money Online 4.0".$t.$red."[°] Dừng Tool --> !!!!".$t.$kuning."[•] Ctrl + C";
+$msgyd = "[!] Việt Hóa Tool".$t."[•] msg By Light-Ning";
+$yd_stat = true;
+$vip1= false;
+$weblock =[""];
+$we_stat = true;
+$msgwe = "[!] Khởi Động Tool";
+$msgveeu = "[!] Bắt Đầu Chạy Tool";
+$data= [
+'Make Money Online 4.0',
+];
+$toto = ['tes',];
+$ydnews = ['tes','premium',];
